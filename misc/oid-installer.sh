24b2277a44649b37939d833a72241a4447c2eda0
jrbalderrama <jrbalderrama@users.noreply.github.com>
2019-06-19 18:54:17 +0200

    Add client installation

--- misc/oid-installer.sh
+++ misc/oid-installer.sh
@@ -2,12 +2,14 @@
 
 OID_DIR=/vagrant
 OIDINTERPRETER_DIR=${OID_DIR}/oidinterpreter
+OSCLI_PLUGIN_DIR=${OID_DIR}/python-openstackoidclient
 OPENDEV_DIR=/opt/opendev
 KEYSTONEAUTH_DIR=${OPENDEV_DIR}/keystoneauth
-KEYSTONEMIDDLEWARE=${OPENDEV_DIR}/keystonemiddleware
+KEYSTONEMIDDLEWARE_DIR=${OPENDEV_DIR}/keystonemiddleware
 
 sudo pip install -U -e ${OIDINTERPRETER_DIR}
-sudo pip install -U -e ${KEYSTONEMIDDLEWARE}
+sudo pip install -U -e ${OSCLI_PLUGIN_DIR}
+sudo pip install -U -e ${KEYSTONEMIDDLEWARE_DIR}
 sudo pip install -U -e ${KEYSTONEAUTH_DIR}
 
 sudo systemctl restart devstack@*
