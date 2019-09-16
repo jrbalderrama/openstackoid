5f601a4967fb8a4d3b23b99ae7a8dfa7cf540661
jrbalderrama <jrbalderrama@users.noreply.github.com>
2019-09-16 18:21:42 +0200

    Add @scope decorator example + Set interpreter in kwargs

--- openstackoid/dispatcher.py
+++ openstackoid/dispatcher.py
@@ -22,7 +22,7 @@ SERVICES_CATALOG_PATH = "file:///etc/openstackoid/catalog.json"
 FILTERED = (ast.Load, ast.And, ast.Or, ast.BitOr, ast.BitAnd, ast.BitXor)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 
 def to_str(node: ast.AST) -> Optional[str]:
@@ -75,12 +75,14 @@ T = TypeVar("T")
 class ScopeTransformer(ast.NodeTransformer, Generic[T]):
 
     def __init__(self,
+                 interpreter: OidInterpreter,
                  func: Callable[..., T],
                  bool_evl_func: Callable[..., bool],
                  args_xfm_func: Callable[..., Tuple[Tuple, Dict]],
                  disj_res_func: Callable[..., T],
                  conj_res_func: Callable[..., T],
                  *arguments, **keywords):
+        self.interpreter = interpreter
         self.func: Callable[..., T] = func
         self.bool_evl_func: Callable[..., bool] = bool_evl_func
         self.args_xfm_func: Callable[..., Tuple[Tuple, Dict]] = args_xfm_func
@@ -91,7 +93,8 @@ class ScopeTransformer(ast.NodeTransformer, Generic[T]):
 
     def visit_Name(self, node):
         logger.info(f"Processing '{node.id}'")
-        return OidDispatcher(node.id,
+        return OidDispatcher(self.interpreter,
+                             node.id,
                              self.func,
                              self.bool_evl_func,
                              self.args_xfm_func,
@@ -105,14 +108,16 @@ class ScopeTransformer(ast.NodeTransformer, Generic[T]):
         operator = "__{}__".format(node.op.__class__.__name__[3:].lower())
         result = None
         if hasattr(node, 'left') and hasattr(node, 'right'):
-            logger.info(f"Evaluating ({node.left} {operator[2:-2]} {node.right})")
             result = getattr(node.left, operator)(node.right)
+            logger.info(f"Evaluating ({node.left} {operator[2:-2]} {node.right})")
+            logger.info(f"Result: {result}")
         else:
             child_node = node.left if hasattr(node, 'left') else node.right
             empty_node = OidDispatcher(
-                None, lambda x: x, lambda x: False, None, None, None, None)
+                None, None, lambda x: x, lambda x: False, None, None, None, None)
             result = getattr(child_node, operator)(empty_node)
-
+            logger.info(f"Evaluating ({child_node} {operator[2:-2]} {empty_node})")
+            logger.info(f"Result: {result}")
         return result
 
 
@@ -122,13 +127,16 @@ default_conj_func=lambda *x: x[1] if x[0] and x[1] else None
 
 class OidDispatcher(Generic[T]):
 
-    def __init__(self, endpoint: str,
+    def __init__(self,
+                 interpreter: OidInterpreter,
+                 endpoint: str,
                  func: Callable[..., T],
                  bool_evl_func: Callable[..., bool],
                  args_xfm_func: Callable[..., Tuple[Tuple, Dict]],
                  disj_res_func: Callable[..., T],
                  conj_res_func: Callable[..., T],
                  *arguments, **keywords):
+        self.interpreter = interpreter
         self.endpoint = endpoint
         self.func: Callable[..., T] = func
         self.bool_evl_func: Callable[..., bool] = bool_evl_func
@@ -142,8 +150,7 @@ class OidDispatcher(Generic[T]):
     @property
     def result(self) -> Optional[T]:
         if not self._result:
-            self.keywords.update(endpoint=self.endpoint)
-            self._result = OidDispatcher[T].func_wrapper(
+            self._result = self.func_wrapper(
                 self.func, self.args_xfm_func,
                 *self.arguments, **self.keywords)
 
@@ -163,13 +170,14 @@ class OidDispatcher(Generic[T]):
         return self.conj_res_func(self, other)
 
     def __str__(self) -> str:
-        return self.endpoint if self.endpoint else 'None'
+        return f"{self.endpoint}({self.result})" if self.endpoint else 'None'
+        #return str(self.result)
 
-    @staticmethod
-    def func_wrapper(func: Callable[..., T],
+    def func_wrapper(self, func: Callable[..., T],
                      args_xfm_func: Callable[..., Tuple[Tuple, Dict]],
                      *arguments, **keywords) -> Optional[T]:
-        args, kwargs = args_xfm_func(*arguments, **keywords)
+        args, kwargs = args_xfm_func(self.interpreter, self.endpoint,
+                                     *arguments, **keywords)
         result: T = func(*args, **kwargs)
         logger.debug(result)
         return result
@@ -184,34 +192,33 @@ class OidDispatcher(Generic[T]):
         def decorator(func:Callable):
             @functools.wraps(func)
             def wrapper(*arguments, **keywords):
-                scope = extr_scp_func(*arguments + (interpreter,), **keywords)
+                scope = extr_scp_func(interpreter, *arguments, **keywords)
                 tree = ast.parse(scope, mode ='eval')
                 # logger.debug(f"\t= {dump(tree)}")
                 visit(tree.body)
                 dispatcher: OidDispatcher[T] = ScopeTransformer[T](
+                    interpreter,
                     func,
                     bool_evl_func,
                     args_xfm_func,
                     disj_res_func,
                     conj_res_func,
-                    *arguments + (interpreter,), **keywords).visit(tree.body)
+                    *arguments, **keywords).visit(tree.body)
                 logger.info(f"\t= {dispatcher}")
                 return dispatcher.result if dispatcher else None
             return wrapper
         return decorator
 
 
-def _get_interpreter(*arguments) -> OidInterpreter:
-    return next(a for a in arguments if isinstance(a, OidInterpreter))
-
-
 def _get_request(*arguments) -> PreparedRequest:
     return next(a for a in arguments if isinstance(a, PreparedRequest))
 
 
-def requests_extr_scp_func(*arguments, **keywords) -> str:
-    interpreter = _get_interpreter(*arguments)
+def requests_extr_scp_func(interpreter: OidInterpreter,
+                           *arguments, **keywords) -> str:
     request = typing.cast(Request, _get_request(*arguments))
+
+    # TODO refactor in interpreter following three lines
     global_scope = interpreter.get_scope(request)
     target_service = interpreter.is_scoped_url(request)
     narrow_scope = global_scope[target_service.service_type]
@@ -219,9 +226,10 @@ def requests_extr_scp_func(*arguments, **keywords) -> str:
     return narrow_scope
 
 
-def requests_args_xfm_func(*arguments, **keywords) -> Tuple[Tuple, Dict]:
-    interpreter = _get_interpreter(*arguments)
-    endpoint = keywords.pop('endpoint')
+def requests_args_xfm_func(interpreter, endpoint,
+                           *arguments, **keywords) -> Tuple[Tuple, Dict]:
+    # TODO remove next() to set args in return
+    # TODO remove atomic_scope (?) see TODO from requests_extr_scp_func
     session = next(a for a in arguments if isinstance(a, Session))
     _request = typing.cast(Request, _get_request(*arguments))
 
--- openstackoid/tests/functional/dispatcher.py
+++ openstackoid/tests/functional/dispatcher.py
@@ -13,14 +13,15 @@ import logging
 import urllib3
 
 from openstackoid.interpreter import Service
-from openstackoid.dispatcher import requests_scope
+from openstackoid.dispatcher import requests_scope, OidDispatcher
 
 import openstackoid.interpreter as oid
 
 
 logger = logging.getLogger(__name__)
+
+
 logging.basicConfig(format='\t%(levelname)s\t: %(message)s')
-#logger.setLevel(logging.DEBUG)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # A dummy identity service is required for proper interpretation of scope
@@ -49,9 +50,9 @@ Session.send = requests_scope(interpreter)(Session.send)
 
 # Only Instance2 has [200] response
 #narrow_scope = "Instance1 & ((Instance3 | Instance2) & (Instance1 | Instance3)) | Instance2"
-narrow_scope = "Instance2 & Instance1 & Instance2 | Instance2 & Instance1 | Instance0"
+narrow_scope = "Instance2 & Instance1 & Instance2 | Instance2 & Instance1 | Instance4"
 narrow_scope = "(Instance3 & Instance1) | Instance2"
-narrow_scope = "Instance2 & (Instance3 | Instance1)"
+#narrow_scope = "Instance2 & (Instance3 | Instance1)"
 # narrow_scope = "Instance2 & Instance3 | Instance1"
 # narrow_scope = "Instance3 | Instance2"
 # narrow_scope = "Instance1"
@@ -71,3 +72,74 @@ prepared_request = session.prepare_request(request)
 ## response = session_send(session, prepared_request)
 ##
 response = session.send(prepared_request, allow_redirects=False, verify=False)
+
+
+# Exemple II basic annotation with a simple function and NO interpretation
+def test_extr_scp_func(*args, **kwargs):
+    return 'R|M'
+
+
+def test_bool_evl_func(instance):
+    return True
+
+
+def test_args_xfm_func(intrepreter, endpoint,
+                       *args, **kwargs):
+    return args, kwargs
+
+
+def test_disj_func(this, other):
+    this.result = this.result + other.result
+    return this
+
+
+def test_conj_func(this, other):
+    this.result = this.result * other.result
+    return this
+
+
+@OidDispatcher.scope(interpreter,
+                     extr_scp_func=test_extr_scp_func,
+                     bool_evl_func=test_bool_evl_func,
+                     args_xfm_func=test_args_xfm_func,
+                     disj_res_func=test_disj_func,
+                     conj_res_func=test_conj_func)
+def non_factorial(n):
+    f = 1
+    while(n):
+        f = f * n
+        n = n - 1
+    return f
+
+
+@OidDispatcher.scope(interpreter,
+                     extr_scp_func=test_extr_scp_func,
+                     bool_evl_func=test_bool_evl_func,
+                     args_xfm_func=test_args_xfm_func,
+                     disj_res_func=test_disj_func,
+                     conj_res_func=test_conj_func)
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+
+@OidDispatcher.scope(interpreter,
+                     extr_scp_func=test_extr_scp_func,
+                     bool_evl_func=test_bool_evl_func,
+                     args_xfm_func=test_args_xfm_func,
+                     disj_res_func=test_disj_func,
+                     conj_res_func=test_conj_func)
+def tail_factorial(n, r = 1):
+    if n <= 1:
+        return r
+    return tail_factorial(n - 1, n * r)
+
+n=3
+
+print("Non factorial")
+r = non_factorial(n)
+
+print("Factorial")
+r = factorial(n)
+
+print("Tail factorial")
+r = tail_factorial(n)
