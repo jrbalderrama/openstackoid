51da232570ad51c8c9702d9d783c2f60562ae3cc
jrbalderrama <jrbalderrama@users.noreply.github.com>
2019-09-25 18:47:41 +0200

    Add external installer for dev

---
+++ misc/openstackoid.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+SUDO_CMD="sudo --set-home"
+PIP_INSTALL_OPTS="--no-deps --no-cache-dir --editable"
+EASY_INSTALL_OPTS="-H None -m"
+
+OS_BASE_DIR=/opt/stack
+OID_BASE_DIR=/opt/opendev
+DIST_PACKAGES_DIR=/usr/local/lib/python3.6/dist-packages
+
+OS_BASE_PATH=${OS_BASE_DIR%%+(/)}${OS_BASE_DIR:+/}
+OID_BASE_PATH=${OID_BASE_DIR%%+(/)}${OID_BASE_DIR:+/}
+DIST_PACKAGES_PATH=${DIST_PACKAGES_DIR%%+(/)}${DIST_PACKAGES_DIR:+/}
+
+OID_CORE="openstackoid"
+KST_MIDL="keystonemiddleware"
+KST_AUTH="keystoneauth"
+OST_CLNT="python-openstackclient"
+GLC_CLNT="python-glanceclient"
+KST_CLNT="python-keystoneclient"
+NVA_CLNT="python-novaclient"
+NET_CLNT="python-neutronclient"
+
+SRC_MODULES=(${KST_MIDL} ${KST_AUTH} ${OST_CLNT} ${GLC_CLNT} ${KST_CLNT} ${NVA_CLNT} ${NET_CLNT})
+set -x
+
+if [ -z "$1" ]
+then
+    ${SUDO_CMD} pip install ${PIP_INSTALL_OPTS} ${OID_BASE_PATH}${OID_CORE}
+    shopt -s nullglob
+    for module in ${SRC_MODULES[@]}
+    do
+        ${SUDO_CMD} pip uninstall --yes $module
+        # required to update easy-install.pth file in dist-packages
+        ${SUDO_CMD} easy_install ${EASY_INSTALL_OPTS} $module
+        for alt_egg in ${DIST_PACKAGES_PATH}${module//-/_}*egg
+        do
+            [ -d $alt_egg ] && ${SUDO_CMD} rm -rf $alt_egg
+        done
+        # required in ubuntu because the package is not uninstalled
+        egg="${DIST_PACKAGES_PATH}${module}.egg-link"
+        [ -f $egg ] && ${SUDO_CMD} rm -f $egg
+        egg_info="${OS_BASE_PATH}${module}/${module//-/_}.egg-info"
+        [ -d $egg_info ] && ${SUDO_CMD} rm -rf $egg_info
+        ${SUDO_CMD} pip install ${PIP_INSTALL_OPTS} ${OID_BASE_PATH}$module
+    done
+    shopt -u nullglob
+else
+    ${SUDO_CMD} pip uninstall --yes ${OID_CORE}
+    ${SUDO_CMD} easy_install ${EASY_INSTALL_OPTS} ${OID_CORE}
+    egg="${DIST_PACKAGES_PATH}${OID_CORE}.egg-link"
+    [ -f $egg ] && ${SUDO_CMD} rm -f $egg
+    egg_info="${OID_BASE_PATH}${OID_CORE}/${OID_CORE}.egg-info"
+    [ -d $egg_info ] && ${SUDO_CMD} rm -rf $egg_info
+    for module in ${SRC_MODULES[@]}
+    do
+        ${SUDO_CMD} pip install ${PIP_INSTALL_OPTS} ${OS_BASE_PATH}$module
+    done
+fi
+
+sudo systemctl restart devstack@*
